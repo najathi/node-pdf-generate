4933f1f9a1683ce61e1a0d199bb13ce303d99c89
Najathi <najathi777@gmail.com>
2020-07-31 20:25:39 +0530

    create html to pdf

---
+++ index.js
@@ -0,0 +1,19 @@
+const wkhtmltopdf = require("wkhtmltopdf");
+const fs = require("fs");
+
+// url
+// wkhtmltopdf("http://google.com/", { pageSize: "letter" }).pipe(
+//     fs.createWriteStream("./output/out.pdf")
+// );
+
+// read file
+// wkhtmltopdf(fs.readFileSync("./assets/invoice.html"), {}).pipe(
+//     fs.createWriteStream("out.pdf")
+// );
+
+// another way read file
+wkhtmltopdf(fs.readFileSync("./assets/invoice.html", "utf-8"), {
+    pageWidth: "210",
+    pageHeight: "297",
+    output: "./output/out.pdf",
+});
